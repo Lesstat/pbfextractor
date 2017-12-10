8d8af29abfbf17ef0f28baeea36a3a1188d86737
Florian Barth <florianbarth@gmx.de>
2017-12-10 14:48:04 +0100

    Adapt Filtering and structure to bicycle add writing of file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,54 @@ extern crate osmpbfreader;
 
 mod pbf;
 
+use std::fs::File;
+use std::io::{Write, BufWriter};
+use std::time::SystemTime;
+use std::env::args;
+
 fn main() {
 
-    println!("Hello, world!");
+    let mut a = args();
+    a.next();
+    let input = a.next().expect("No input file given");
+    let output = a.next().expect("No output file given");
+
+
+    let f = File::create(&output).unwrap();
+    let mut b = BufWriter::new(f);
+
+    let (nodes, edges) = pbf::load_graph(input);
+
+    println!("Writing to: {}", output);
+
+    write!(&mut b, "# Build by: pbfextractor\n").unwrap();
+    write!(&mut b, "# Build on: {:?}\n", SystemTime::now()).unwrap();
+    write!(&mut b, "\n").unwrap();
+    write!(&mut b, "{}\n", nodes.len()).unwrap();
+    write!(&mut b, "{}\n", edges.len()).unwrap();
+
+    for (i, node) in nodes.iter().enumerate() {
+        write!(
+            &mut b,
+            "{} {} {} {} 0\n",
+            i,
+            node.osm_id,
+            node.lat,
+            node.long
+        ).unwrap();
+    }
+    for edge in &edges {
+        write!(
+            &mut b,
+            "{} {} {} {} {} -1 -1\n",
+            edge.source,
+            edge.dest,
+            edge.length,
+            edge.height,
+            edge.unsuitability
+        ).unwrap();
+    }
+
+    b.flush().unwrap();
+
 }
--- src/pbf.rs
+++ src/pbf.rs
@@ -2,18 +2,13 @@ use osmpbfreader::{OsmObj, OsmPbfReader, Way};
 
 use std::path::Path;
 use std::fs::File;
-use std::time::Instant;
 
 
 /// Loads the graph from a pbf file.
-///
-/// All edges and nodes that contain a highway tag and are accessible
-/// for either cars or pedestrians by the judgement of is_not_for_cars
-/// and is_not_for_pedestrians
 pub fn load_graph<P: AsRef<Path>>(p: P) -> (Vec<NodeInfo>, Vec<EdgeInfo>) {
+    println!("Extracting data out of: {}", p.as_ref().to_str().unwrap());
     let fs = File::open(p).unwrap();
     let mut reader = OsmPbfReader::new(fs);
-    let start_loading = Instant::now();
     let obj_map = reader
         .get_objs_and_deps(|obj| obj.tags().contains_key("highway"))
         .unwrap();
@@ -27,42 +22,31 @@ pub fn load_graph<P: AsRef<Path>>(p: P) -> (Vec<NodeInfo>, Vec<EdgeInfo>) {
                     node.id.0 as usize,
                     (node.decimicro_lat as f64) / 10_000_000.0,
                     (node.decimicro_lon as f64) / 10_000_000.0,
-                    0,
                 ));
             }
             OsmObj::Way(w) => {
-                let speed = determine_speed(&w);
+                if is_not_for_bicycle(&w) {
+                    continue;
+                }
+                let unsuitability = determine_unsuitability(&w);
                 let is_one_way = is_one_way(&w);
-                let no_cars = is_not_for_cars(&w);
-                let no_pedestrians = is_not_for_pedestrians(&w);
                 for (index, node) in w.nodes[0..(w.nodes.len() - 1)].iter().enumerate() {
-                    let mut edge = EdgeInfo::new(
+                    let edge = EdgeInfo::new(
                         node.0 as NodeId,
                         w.nodes[index + 1].0 as NodeId,
                         1.1, // calculating length happens inside the graph
-                        speed,
+                        1.1,
+                        unsuitability,
                     );
-                    if no_cars && no_pedestrians {
-                        continue;
-                    }
-                    if no_cars {
-                        edge.not_for_cars();
-                    } else if no_pedestrians {
-                        edge.not_for_pedestrians();
-                    }
                     edges.push(edge);
                     if !is_one_way {
-                        let mut edge = EdgeInfo::new(
+                        let edge = EdgeInfo::new(
                             w.nodes[index + 1].0 as NodeId,
                             node.0 as NodeId,
                             1.1, // calculating length happens inside the graph
-                            speed,
+                            1.1,
+                            unsuitability,
                         );
-                        if no_cars {
-                            edge.not_for_cars();
-                        } else if no_pedestrians {
-                            edge.not_for_pedestrians();
-                        }
                         edges.push(edge);
                     }
                 }
@@ -70,24 +54,47 @@ pub fn load_graph<P: AsRef<Path>>(p: P) -> (Vec<NodeInfo>, Vec<EdgeInfo>) {
             _ => (),
         }
     }
-    println!("Amount of Edges {}", edges.len());
 
+    println!("Amount of Edges {}", edges.len());
     return (nodes, edges);
 }
 
-fn determine_speed(way: &Way) -> Speed {
-    let speed = way.tags.get("maxspeed").and_then(|s| s.parse().ok());
-    if speed.is_some() {
-        speed.unwrap()
-    } else {
-        let street_type = way.tags.get("highway").map(String::as_ref);
-        match street_type {
-            Some("motorway") => 130,
-            Some("residential") => 50,
-            _ => 100,
-        }
+fn determine_unsuitability(way: &Way) -> Unsuitability {
+    if way.tags.get("cycleway").is_some() || way.tags.get("bicycle") == Some(&"yes".to_string()) {
+        return 0;
     }
+
+    let side_walk: Option<&str> = way.tags.get("sidewalk").map(String::as_ref);
+    if side_walk == Some("yes") {
+        return 1;
+    }
+
+    let street_type = way.tags.get("highway").map(String::as_ref);
+    match street_type {
+        Some("primary") => 5,
+        Some("primary_link") => 5,
+        Some("secondary") => 4,
+        Some("secondary_link") => 4,
+        Some("tertiary") => 3,
+        Some("tertiary_link") => 3,
+        Some("road") => 3,
+        Some("bridleway") => 3,
+        Some("unclassified") => 2,
+        Some("residential") => 2,
+        Some("traffic_island") => 2,
+        Some("living_street") => 1,
+        Some("service") => 1,
+        Some("track") => 1,
+        Some("platform") => 1,
+        Some("pedestrian") => 1,
+        Some("path") => 1,
+        Some("footway") => 1,
+        Some("cycleway") => 0,
+        _ => 10,
+    }
+
 }
+
 fn is_one_way(way: &Way) -> bool {
     let one_way = way.tags.get("oneway").and_then(|s| s.parse().ok());
     match one_way {
@@ -100,24 +107,12 @@ fn is_one_way(way: &Way) -> bool {
         }
     }
 }
-fn is_not_for_cars(way: &Way) -> bool {
-    let street_type = way.tags.get("highway").map(String::as_ref);
-    match street_type {
-        Some("footway") |
-        Some("bridleway") |
-        Some("steps") |
-        Some("path") |
-        Some("cycleway") |
-        Some("track") |
-        Some("proposed") |
-        Some("construction") |
-        Some("pedestrian") => true,
-        _ => false,
+
+fn is_not_for_bicycle(way: &Way) -> bool {
+
+    if way.tags.get("cycleway").is_some() || way.tags.get("bicycle") == Some(&"yes".to_string()) {
+        return false;
     }
-
-}
-
-fn is_not_for_pedestrians(way: &Way) -> bool {
 
     let street_type = way.tags.get("highway").map(String::as_ref);
     let side_walk: Option<&str> = way.tags.get("sidewalk").map(String::as_ref);
@@ -130,10 +125,18 @@ fn is_not_for_pedestrians(way: &Way) -> bool {
     }
     match street_type {
         Some("motorway") |
+        Some("motorway_link") |
         Some("trunk") |
+        Some("trunk_link") |
         Some("proposed") |
-        Some("construction") |
-        Some("primary") => true,
+        Some("path") |
+        Some("footway") |
+        Some("steps") |
+        Some("elevator") |
+        Some("corridor") |
+        Some("raceway") |
+        Some("rest_area") |
+        Some("construction") => true,
         _ => false,
     }
 
@@ -144,23 +147,21 @@ pub type OsmNodeId = usize;
 pub type Latitude = f64;
 pub type Longitude = f64;
 pub type Length = f64;
-pub type Speed = usize;
-pub type Height = usize;
+pub type Height = f64;
+pub type Unsuitability = usize;
 
 pub struct NodeInfo {
     pub osm_id: OsmNodeId,
     pub lat: Latitude,
     pub long: Longitude,
-    pub height: Height,
 }
 
 impl NodeInfo {
-    pub fn new(osm_id: OsmNodeId, lat: Latitude, long: Longitude, height: Height) -> NodeInfo {
+    pub fn new(osm_id: OsmNodeId, lat: Latitude, long: Longitude) -> NodeInfo {
         NodeInfo {
             osm_id: osm_id,
             lat: lat,
             long: long,
-            height: height,
         }
     }
 }
@@ -168,30 +169,26 @@ impl NodeInfo {
 pub struct EdgeInfo {
     pub source: NodeId,
     pub dest: NodeId,
-    length: Length,
-    speed: Speed,
-    for_cars: bool,
-    for_pedestrians: bool,
+    pub length: Length,
+    pub height: Height,
+    pub unsuitability: Unsuitability,
 }
 
 impl EdgeInfo {
-    pub fn new(source: NodeId, dest: NodeId, length: Length, speed: Speed) -> EdgeInfo {
+    pub fn new(
+        source: NodeId,
+        dest: NodeId,
+        length: Length,
+        height: Height,
+        unsuitability: Unsuitability,
+    ) -> EdgeInfo {
+
         EdgeInfo {
             source: source,
             dest: dest,
             length: length,
-            speed: speed,
-            for_cars: true,
-            for_pedestrians: true,
+            height: height,
+            unsuitability: unsuitability,
         }
-    }
-
-    /// Prevent routes for cars from using this edge
-    pub fn not_for_cars(&mut self) {
-        self.for_cars = false;
-    }
-    /// Prevent routes for pedestrians from using this edge
-    pub fn not_for_pedestrians(&mut self) {
-        self.for_pedestrians = false;
     }
 }
